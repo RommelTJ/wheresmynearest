a9cfd30c66f44b9553c048df12cade98af82f17a
Rommel Rico <rommeltj@gmail.com>
2015-04-18 22:17:43 -0700

    Initial iPhone GUI and default values

--- wheresmynearest/ViewController.swift
+++ wheresmynearest/ViewController.swift
@@ -10,9 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var defaults = NSUserDefaults(suiteName: "group.com.rommelrico.wheresmynearest")
+    var places = [String]()
+    
+    @IBOutlet weak var place1: UITextField!
+    @IBOutlet weak var place2: UITextField!
+    @IBOutlet weak var place3: UITextField!
+    @IBOutlet weak var place4: UITextField!
+    
+    @IBAction func doUpdate(sender: AnyObject) {
+        //TODO
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var storedPlaces: AnyObject? = defaults?.objectForKey("places")
+        if let storedPlacesArray = storedPlaces as? NSArray {
+            for (index, value) in enumerate(storedPlacesArray) {
+                if let placeName = value as? String {
+                    places.append(placeName)
+                }
+            }
+        }
+        
+        if places.count == 0 {
+            places = ["cafe", "library", "bar", "restaurant"]
+        }
+        
+        place1.text = places[0]
+        place2.text = places[1]
+        place3.text = places[2]
+        place4.text = places[3]
+        
     }
 
     override func didReceiveMemoryWarning() {
